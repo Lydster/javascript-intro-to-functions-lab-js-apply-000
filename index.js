882a9ecca27fb52d4573a246e2c9a7fd97b3e172
Lydia Thornton <lydia.d.thornton@gmail.com>
2018-08-15 18:14:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function whisper(string) {
 
 function logShout(string) {
   if (uppercase === uppercase.toUpperCase) {
-  console.log(string)
+  console.log(uppercase)
     
   }
 }
@@ -22,13 +22,5 @@ function logWhisper(string) {
   console.log(string.toLowerCase)
 }
 
-function sayHiToGrandma(string) {
-  if (string.toLowerCase === string) {
-    return "I can't hear you!";
-  } else if (string.toUpperCase === string) {
-    return "YES INDEED!";
-  } else {
-    return "I love you, too.";
-  }
-}
+
 
