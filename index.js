46e6e7e9a8db22f1f127c9d29e5ff4ebfbc06b21
Lydia Thornton <lydia.d.thornton@gmail.com>
2018-08-15 18:00:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 function shout(string) {
   return string.toUpperCase()
 }
+
+function Whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase)
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase)
+}
+
